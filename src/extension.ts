d967c53211046fb246f402179dee3e60ee28432e
Tristan Gerritsen <code@thewoosh.org>
2023-10-20 12:57:44 +0200

    Add basic insertion on new files

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,60 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import fs = require('fs');
+
+interface FileInfo {
+	extension: string,
+	text: string
+}
+
+function debug(text: string) {
+	// console.log(text);
+	// vscode.window.showInformationMessage(text);
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let newFiles: string[] = [];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copyright" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('copyright.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from copyright!');
+	vscode.workspace.onDidCreateFiles(e => {
+		newFiles = e.files.map(x => x.fsPath);
+		debug(`Did Create Files: ${JSON.stringify(newFiles)}`);
 	});
 
-	context.subscriptions.push(disposable);
+	vscode.workspace.onDidOpenTextDocument(e => {
+		debug('Did Open Text Document');
+		if (e.isUntitled) {
+			debug('Sorry: untitled');
+			return;
+		}
+
+		const path = e.uri.fsPath;
+		if (!newFiles.includes(path)) {
+			debug(`Sorry: not a new file: ${JSON.stringify(path)}`);
+			return;
+		}
+
+		const text = e.getText();
+		if (text.trim().length !== 0) {
+			debug(`Sorry: not empty: ${text}`);
+			return;
+		}
+
+		const config = vscode.workspace.getConfiguration("copyright");
+		debug(`Config: ${JSON.stringify(config)}`);
+		console.log();
+
+		const configData = config.get("files") as FileInfo[];
+		debug(`Data: ${JSON.stringify(configData)}`);
+		for (const info of configData) {
+			if (path.endsWith(info.extension)) {
+				fs.writeFileSync(e.uri.fsPath, info.text);
+				return;
+			}
+		}
+	})
 }
 
 // This method is called when your extension is deactivated
